eb2b2c62f105e1361d2d2df3e4f3543141ac18b6
kaprosdev <7116426+kaprosdev@users.noreply.github.com>
2024-04-13 13:51:44 -0400

    initial commit v2

---
+++ RichFezence.cs
@@ -0,0 +1,13 @@
+﻿using FezEngine.Tools;
+using Microsoft.Xna.Framework;
+using System;
+
+namespace RichFEZence
+{
+	public class RichFezence : DrawableGameComponent
+	{
+		public RichFezence(Game game) : base(game)
+		{
+		}
+	}
+}
